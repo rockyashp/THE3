c676cab6a3dfde935b34bcac4e06416291e4c4b5
ashwinagrawal30 <ashwinagrawal400@gmail.com>
2026-06-02 12:45:13 +0530

    #2 issue2

---
+++ Directory.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Creating directory structure..."
+
+mkdir -p India/States/{Maharashtra,Gujarat,Rajasthan,Kerala,TamilNadu}
+mkdir -p India/Tourist/{TajMahal,GatewayOfIndia,JaipurCity,Hampi,GoaBeaches}
+
+echo "Directory structure created successfully."
+
